8514bb933c3c78da25c04fdcb92c4f1b246dc1ef
ist176471 <jmmmdfa@gmail.com>
2016-03-10 11:28:19 +0000

    Added files via upload

---
+++ TestClient.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import ClientPack.*;
+
+/**
+ *  Test suite
+ */
+public class TestClient {
+
+	private Client client;
+	
+    public void setUp(){
+		this.client= new Client();
+		client.FS_init();
+    }
+    /*
+    public void success() {
+    }
+	
+	
+    */
+	
+}
